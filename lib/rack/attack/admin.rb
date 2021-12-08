5610fb78629992796ae2932fbdb72aa5ac19d4cb
Murray Summers <murray@tito.io>
2021-12-08 16:42:01 +0000

    It is a class.

--- lib/rack/attack/admin.rb
+++ lib/rack/attack/admin.rb
@@ -2,7 +2,7 @@ require "rack/attack/admin/version"
 require "rack/attack/admin/blocked_ip_address"
 
 module Rack
-  module Attack
+  class Attack
     module Admin
       class Error < StandardError; end
       # Your code goes here...
